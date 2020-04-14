7ce929a0197b7a96c5cdf573d15aa9cb2a3abb29
Patrick <patrick.hurley@codeimmersives.com>
2020-04-13 20:58:58 -0400

    finished

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const Stack = function() {
-}
+  return{
+    items: [],
+    add: function(name){
+      this.items.push(name);
+    },
+    remove: function(){
+      return this.items.pop();
+    },
+    peek: function(){
+      return this.items[this.items.length-1];
+    },
+  };
+};
 
 const Queue = function() {
-}
+  return{
+    items: [],
+    add: function (name){
+      return this.items.push(name);
+    },
+    remove: function(){
+      return this.items.shift();
+    },
+    peek: function(){
+      return this.items[0];
+    },
+  };
+};
 
 module.exports = {
   Stack,
